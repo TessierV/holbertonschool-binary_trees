87a4d9fecb62fbbae2596f40373b36dcd03105a2
TessierV <5628@holbertonstudents.com>
2023-01-09 04:41:38 -0800

    saving exercice 7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - go trough a binary tree using pre-order traversal
+ * @tree: binary tree
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	if (tree->left)
+	{
+		binary_tree_inorder(tree->left, func);
+	}
+	if (tree->right)
+		binary_tree_inorder(tree->right, func);
+}
