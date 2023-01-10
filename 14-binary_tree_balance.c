44fe5072a8c7cc224c7aa5bc1c0729ac6d98dd4a
marlyne <=>
2023-01-10 17:24:56 +0100

    Balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,19 +7,24 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l_height = 0, r_height = 0;
+	size_t left_height;
+	size_t right_height;
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		l_height = binary_tree_height(tree->left) + 1;
-	if (tree->right)
-		r_height = binary_tree_height(tree->right) + 1;
-	if (l_height < r_height)
-		return (r_height);
-	return (l_height);
-}
 
+	/* checks if it has any child */
+	if (!tree->left && !tree->right)
+		return (1);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
 
 /**
  * binary_tree_balance - measure the balance factor of binary tree
